aeb5313da3589d283d1e9a05809b7e763a3573a6
kus <kushagra746@gmail.com>
2025-07-02 22:00:11 +0530

    new

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,7 +3,6 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('User route is working');
+    res.send('User route is done');
     });
 
-    
